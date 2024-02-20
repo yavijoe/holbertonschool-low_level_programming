81b2d6d7e511153c1df41fb6d1408a391f47b111
Yavier Maldonado <yavi@Yavis-MacBook-Pro.local>
2024-02-20 19:35:51 -0400

    Betty has left the chat

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - I'll make a program that prints a line using put function
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
