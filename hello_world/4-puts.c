ed0b3435beb83e06fcebaf5ba3a1d91d34699044
Yavier Maldonado <yavi@Yavis-MacBook-Pro.local>
2024-02-21 19:48:11 -0400

    Forgot to add the tab

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts
- * return 0
- *
- * int main(void)
- * {
- *	puts("Programming is like building a multilingual puzzle)"
- *	return (0)
- *	}
+ * main - A program that prints a line with the puts function
+ * Return: Always  0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
