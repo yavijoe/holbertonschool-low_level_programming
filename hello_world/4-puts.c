5e4d675fedb177ca2972d7f297ca9e3b6e59ec30
Yavier Maldonado <yavi@Yavis-MBP.localdomain>
2024-02-21 14:53:05 -0400

    Hope this worked

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - A programm that prints a line with the puts function
- * Return: 0;
- */
-int main(void)
-{
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
-}
+ * main - A program that prints a line with puts
+ * return 0
+ *
+ * int main(void)
+ * {
+ *	puts("Programming is like building a multilingual puzzle)"
+ *	return (0)
+ *	}
