d04b87c0be598d5ab425f1f75a6eec33608dcd77
Yavier Maldonado <yavi@Yavis-MacBook-Pro.local>
2024-02-21 23:09:03 -0400

    Im going to sleep

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include "main.h"
+#include"main.h"
+
 /**
-* main - Print "_putchar" folloed by a new line.
-*
-* Description: You are not allowed to include standard libraries.
-* Return: 0
-*/
+ * main - Entry point of my code
+ * Return: Always 0
+ */
 int main(void)
 {
-	char putchar[] = "_putchar";
-	int i = 0;
-
-	while (putchar[i] != '\0')
-	{
-		_putchar(putchar[i]);
-		i++;
-	}
-	_putchar ('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
