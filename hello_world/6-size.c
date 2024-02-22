ca6ed9535bf9d7e756db8ac899ffb4d2dbb8b626
Yavier Maldonado <yavi@Yavis-MacBook-Pro.local>
2024-02-21 20:29:45 -0400

    I need to keep going pls

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry points
  *
@@ -6,16 +6,10 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("Size of a char: %lu  byte (s)\n, (unsigned long)sizeof (a));
-	printf("Size of an int : % d byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: % d byte(s)\n",(unsigned long)sizeof(c));
-	printf("Size of a long long int: % d byte(s)\n",(unsigned long)sizeof(d));
-	printf("Size of a float : % d byte(s)\n",(unsigned long)sizeof(f));
+	printf("Size of a char: %lu  byte (s)\n, sizeof(char));
+	printf("Size of an int : % d byte(s)\n", sizeof(int));
+	printf("Size of a long int: % d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: % d byte(s)\n", sizeof(long long int));
+	printf("Size of a float : % d byte(s)\n", sizeof(float));
 	return (0);
 }
