6b918916a89bc01ba7de4847ffb7135a46a84c9e
Yavier Maldonado <yavi@Yavis-MBP.localdomain>
2024-02-22 13:41:21 -0400

    0 putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include "main.h"
-#include <unistd.h>
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char putchar[] = "_putchar";
-	int i = 0;
-
-	while (putchar[i] != '\0')
-	{
-		 _putchar(putchar[i]);
-		 i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
 }
-
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
-
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,6 +10,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c);
-
-#endif
+void print_times_table(int n);
