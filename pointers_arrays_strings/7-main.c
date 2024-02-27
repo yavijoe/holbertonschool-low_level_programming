94ffaca78ceafed0367685d1d810a2dffcc7797a
Yavier Maldonado <yavi@Yavis-MacBook-Pro.local>
2024-02-26 22:18:04 -0400

    Yes worked did it?

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -7,10 +7,33 @@
  */
 int main(void)
 {
-	char *str;
+    char *str;
 
-
-	str = "0123456789";
-	puts_half(str);
-	return (0);
+    str = "0123456789";
+    puts_half(str);
+    return (0);
 }
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: the string to print
+ */
+void puts_half(char *str)
+{
+    int length = 0;
+    int i;
+
+    /* Calculate the length of the string */
+    while (str[length] != '\0')
+    {
+        length++;
+    }
+
+    /* Print the second half of the string */
+    for (i = length / 2; i < length; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+}
+
