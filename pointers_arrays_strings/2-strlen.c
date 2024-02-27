3419f32dfb1883f8a96bf3a4a086aa9a40a4fe96
Yavier Maldonado <yavi@Yavis-MacBook-Pro.local>
2024-02-26 21:21:01 -0400

    Will this be rigth?

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s++)
+len++;
+
+return (len);
+}
