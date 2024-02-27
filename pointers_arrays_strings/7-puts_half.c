6f1a26f21a4d3839836870e5831445af35171244
Yavier Maldonado <yavi@Yavis-MacBook-Pro.local>
2024-02-26 22:33:37 -0400

    Found the error on #7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,29 +1,30 @@
 #include "main.h"
 /**
- * puts2 - function should print only one character out of two
- * starting with the first one
- * @str: input
- * Return: print
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
-
-	while (*y != '\0')
-	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-	if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
-	}
-	_putchar('\n');
+int len, n, i;
+len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+else if (len % 2)
+{
+for (n = (len - 1) / 2; n < len - 1; n++)
+{
+_putchar(str[n + 1]);
+}
+}
+_putchar('\n');
 }
