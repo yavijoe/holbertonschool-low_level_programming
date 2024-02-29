5da893f74a9207a6b7adc45df475784b23e784c4
Yavier Maldonado <yavi@Yavis-MacBook-Pro.local>
2024-02-29 16:35:33 -0400

    Forgot to compile it

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,44 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * _strstr - prints the consecutive caracters of s1 that are in s2.
- * @haystack: source string
- * @needle: searching string
- *
- * Return: new string.
+ * _strstr - function that locates a substring
+ * @haystack: string to be searched
+ * @needle: string to be located
+ * Return: pointer to the beginning of the located substring
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
-	{
-		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
-		{
-			return (haystack);
-		}
-		else
-		{
-			haystack++;
-		}
-	}
-	return (0);
-}
-/**
- * coincidence - define if the string b is inside a.
- * @a: source string
- * @b: string to be searched
- *
- * Return: 1 if there is coincidence, otherwise 0.
- */
-int coincidence(char *a, char *b)
+int i, j;
+for (i = 0; haystack[i] != '\0'; i++)
 {
-	while (*b && *b == *a)
-	{
-		b++;
-		a++;
-	}
-
-	if (*b == '\0')
-		return (1);
-	else
-		return (0);
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+{
+break;
+}
+}
+if (!needle[j])
+{
+return (haystack + i);
+}
+}
+return (0);
 }
